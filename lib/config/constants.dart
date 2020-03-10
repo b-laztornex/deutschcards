f5bf84f1e4a0b1d53c853195063d4f8bc30ef739
Blaztornex <m.casanova.dev@gmail.com>
2020-03-10 18:00:55 +0100

    add constants cardinfo

---
+++ lib/config/constants.dart
@@ -0,0 +1,12 @@
+import 'package:german_card/model/card.dart';
+
+class AppConstants {
+  List<CardItem> cardItems = [
+    CardItem('abschließen','er schließt ab','schloss ab','hat abgeschlossen','verben','Cerrar algo con llave, terminar algo ',''),
+    CardItem('anbieten','er schließt ab','schloss ab','hat abgeschlossen','verben','Cerrar algo con llave, terminar algo ',''),
+    CardItem('anfangen','er schließt ab','schloss ab','hat abgeschlossen','verben','Cerrar algo con llave, terminar algo ',''),
+    CardItem('anrufen','er schließt ab','schloss ab','hat abgeschlossen','verben','Cerrar algo con llave, terminar algo ',''),
+    CardItem('anschließen','er schließt ab','schloss ab','hat abgeschlossen','verben','Cerrar algo con llave, terminar algo ',''),
+    CardItem('anziehen (sich)','er schließt ab','schloss ab','hat abgeschlossen','verben','Cerrar algo con llave, terminar algo ',''),
+  ];
+}
